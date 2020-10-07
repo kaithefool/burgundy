1e474148c3ab03ccd8cbf48e4553f6740e19a34f
Kai Lam <kailam@4d.com.hk>
2020-10-07 11:02:42 +0800

    express

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "env": {
+    "node": true,
+  },
+  "extends": "airbnb-base",
+  "rules": {
+    // enable development in windows
+    "linebreak-style": "off",
+    // misc
+    "class-methods-use-this": "off",
+    "import/prefer-default-export": "off",
+    "no-param-reassign": ["error", { "props": false }],
+  },
+}
