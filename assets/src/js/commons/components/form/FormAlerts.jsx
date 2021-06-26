5d43a6ed2c3ae5d23b51b28bc2ad91b7e320a85e
Kai Lam <kai.chun.lam@gmail.com>
2021-06-26 23:53:08 +0800

    fix form validation alert

--- assets/src/js/commons/components/form/FormAlerts.jsx
+++ assets/src/js/commons/components/form/FormAlerts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useFormikContext } from 'formik';
 
 import useFormHttp from './useFormHttp';
