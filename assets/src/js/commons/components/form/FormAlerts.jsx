0488aada0ca6477c4013a2764d69ed732ae2bc0f
Kai Lam <kai.chun.lam@gmail.com>
2021-06-26 23:29:28 +0800

    fix form validation alert

--- assets/src/js/commons/components/form/FormAlerts.jsx
+++ assets/src/js/commons/components/form/FormAlerts.jsx
@@ -5,26 +5,19 @@ import useFormHttp from './useFormHttp';
 import { useAlert } from '../alert';
 
 const FormAlerts = () => {
-  const [changed, setChanged] = useState(false);
-  const { isValidating, errors } = useFormikContext();
+  const { submitCount, isValid } = useFormikContext();
   const formHttp = useFormHttp();
   const { push } = useAlert(formHttp);
 
   useEffect(() => {
-    if (
-      changed
-      && isValidating === false
-      && Object.keys(errors).length
-    ) {
+    if (!isValid) {
       push({
         dirty: true,
         theme: 'danger',
         children: 'Hmmm...you\'ve to complete the form.',
       });
     }
-
-    setChanged(true);
-  }, [isValidating]);
+  }, [submitCount]);
 
   return <></>;
 };
