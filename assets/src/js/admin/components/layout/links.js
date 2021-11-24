798aef493fe32bb54cb93871fd285b97ec825a19
Kai Lam <kailam@4d.com.hk>
2021-11-24 11:06:49 +0800

    remove i18n cms

--- assets/src/js/admin/components/layout/links.js
+++ assets/src/js/admin/components/layout/links.js
@@ -1,9 +1,6 @@
 import { faUser as faReUser } from '@fortawesome/free-regular-svg-icons/faUser';
 import { faUserTie } from '@fortawesome/free-solid-svg-icons/faUserTie';
 import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
-import {
-  faGlobeAmericas,
-} from '@fortawesome/free-solid-svg-icons/faGlobeAmericas';
 
 export default [
   [
@@ -26,12 +23,6 @@ export default [
           to: '/admin/users/clients',
         },
       ],
-    },
-  ], [
-    {
-      icon: faGlobeAmericas,
-      label: 'Translations',
-      to: '/admin/i18n',
     },
   ],
 ];
