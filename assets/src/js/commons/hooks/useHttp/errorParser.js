a8c14dade942ef53ae55d4426e0af53dc67e67df
Kai Lam <kailam@4d.com.hk>
2021-11-30 17:32:15 +0800

    i18n connection error msg

--- assets/src/js/commons/hooks/useHttp/errorParser.js
+++ assets/src/js/commons/hooks/useHttp/errorParser.js
@@ -1,11 +1,13 @@
+import i18n from '../../config/i18n';
+
 export default function errorParser(err) {
   const { response, message, code } = err;
   let payload;
 
   if (message === 'Network Error') {
-    payload = { message: 'Oops...can\'t connect to the server :(' };
+    payload = { message: i18n.t('res.disconnected') };
   } else if (code === 'ECONNABORTED') {
-    payload = { message: 'Connection timeout' };
+    payload = { message: i18n.t('res.timeout') };
   } else {
     payload = response.data;
   }
