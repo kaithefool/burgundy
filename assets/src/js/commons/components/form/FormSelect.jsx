6339946218555c7780114e191e3f7862f54d4c2d
Kai Lam <kailam@4d.com.hk>
2021-12-28 11:54:45 +0800

    i18n form select

--- assets/src/js/commons/components/form/FormSelect.jsx
+++ assets/src/js/commons/components/form/FormSelect.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import FormInput from './FormInput';
 
 const FormSelect = ({
+  children,
   ...props
-}) => (
-  <FormInput
-    as="select"
-    fieldClassName="form-select"
-    {...props}
-  />
-);
+}) => {
+  const { t } = useTranslation();
+
+  const c = Array.isArray(children)
+    ? children.map((o) => (
+      <option key={o} value={o}>
+        {t(o, o)}
+      </option>
+    )) : children;
+
+  return (
+    <FormInput
+      as="select"
+      fieldClassName="form-select"
+      {...props}
+    >
+      {c}
+    </FormInput>
+  );
+};
 
 export default FormSelect;
