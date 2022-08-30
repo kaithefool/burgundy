b884c0659a8da9776e8de323464384da0f06d71f
Kai Lam <kailam@4d.com.hk>
2022-08-30 17:07:50 +0800

    export pdf

--- server/api/responders/exportPdf.js
+++ server/api/responders/exportPdf.js
@@ -7,8 +7,6 @@ const { mapRow, mapHeader } = require('./helpers');
 
 applyPlugin(JsPDF);
 
-const demo = '鑑於對人類家庭所有成員的固有尊嚴及其平等的和不移的權利的承認，乃是世界自由、正義與和平的基礎， 鑑於對人權的無視和侮蔑已發展為野蠻暴行，這些暴行沾污了人類的良心，而一個人人享有言論和信仰自由並免予恐懼和匱乏的世界的來臨，已被宣布為普通人民的最高願望， 鑑於為使人類不致迫不得已鋌而走險對暴政和壓迫進行反叛，有必要使人權受法治的保護， 鑑於有必要促進各國間友好關係的發展， 鑑於各聯合國國家的人民已在聯合國憲章中重申他們對基本人權、人格尊嚴和價值以及男女平等權利的信念，並痩心促成較大自由中的社會進步和生活水平的改善， 鑑於各會員國並已誓願同聯合國合作以促進對人權和基本自由的普遍尊重和遵行， 鑑於對這些權利和自由的普遍了解對於這個誓願的充分實現具有很大的重要性';
-
 const fonts = {
   'Noto Sans': {
     normal: {
@@ -82,20 +80,19 @@ module.exports = (options = {}) => async (req, res) => {
     doc.autoTable({
       head: [mapHeader(mapping)],
       body: out.map((o) => mapRow(req, mapping, o)),
-      styles: {
-        font: 'Noto Sans',
-        fontSize: 8,
-      },
-      headStyles: {
-        fillColor: '#333333',
-      },
+      styles: { font: 'Noto Sans', fontSize: 8 },
+      headStyles: { fillColor: '#333333' },
       ...tableOpts,
     });
   };
 
-  doc.setFontSize(14);
-  doc.autoText(title);
-  doc.outTable({ startY: 24 });
+  if (write) {
+    write(doc, out, req);
+  } else {
+    doc.setFontSize(14);
+    doc.text(title, 14, 18);
+    doc.outTable({ startY: 24 });
+  }
 
   const buf = Buffer.from(
     doc.output('arraybuffer'),
