88f235005832ed850914ff7d354636366507880d
Kai Lam <kailam@4d.com.hk>
2022-10-21 12:01:03 +0800

    redirect opts for authorizer

--- assets/src/js/home/components/pages/auth/PagePwdResetSuccess.jsx
+++ assets/src/js/home/components/pages/auth/PagePwdResetSuccess.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome';
-import { faCheckCircle } from '@fortawesome/free-regular-svg-icons/faCheckCircle';
+import {
+  faCheckCircle,
+} from '@fortawesome/free-regular-svg-icons/faCheckCircle';
 import { Link } from 'react-router-dom';
 import Centered from '~/commons/components/layout/Centered';
 import { resolvePath } from '~/commons/helpers';
