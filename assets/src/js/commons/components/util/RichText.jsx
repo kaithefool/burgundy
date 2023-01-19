c023646333b6ce4a94b16f88429b2ad625bc49b0
Kai Lam <kailam@4d.com.hk>
2023-01-19 12:14:15 +0800

    highlight in richtext

--- assets/src/js/commons/components/util/RichText.jsx
+++ assets/src/js/commons/components/util/RichText.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 
 const urlRegex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
 
+const escapeRegExp = (str) => str
+  .trim()
+  .replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
 const RichText = ({
   children = '',
   multilines = true,
   links = false,
+  highlight,
 }) => {
+  const markRegex = highlight
+    && new RegExp(escapeRegExp(highlight), 'i');
+  const markSplitRegex = highlight
+    && new RegExp(`(${escapeRegExp(highlight)})`, 'i');
   let content = [children];
+
+  const mark = (str) => str
+    .split(markSplitRegex)
+    .map((s, i) => (
+      s.match(markRegex) ? <mark key={`mark-${i}`}>{s}</mark> : s
+    ));
 
   if (multilines) {
     content = content.flatMap((c) => (
@@ -37,7 +52,7 @@ const RichText = ({
               target="_blank"
               rel="noreferrer"
             >
-              {s}
+              {highlight ? mark(s) : s}
             </a>,
           );
         } else if (typeof o[last] === 'string') {
@@ -48,6 +63,14 @@ const RichText = ({
       });
 
       return o;
+    });
+  }
+
+  if (highlight) {
+    content = content.flatMap((c) => {
+      if (typeof c !== 'string') return [c];
+
+      return mark(c);
     });
   }
 
