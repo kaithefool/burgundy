263498464d3c45e6cb3dbc17b17b88f159405c87
Kai Lam <kailam@4d.com.hk>
2023-01-19 12:22:50 +0800

    sanitize mongodb injection

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const errHandler = require('./lib/err/errHandler');
 const i18n = require('./start/i18n');
@@ -16,10 +17,15 @@ app.set('views', path.join(__dirname, 'pages/views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({
+  verify: (req, res, buf) => {
+    req.rawBody = buf;
+  },
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(mongoSanitize({ allowDots: true }));
 
 app.use(i18n);
 app.use('/api', api);
