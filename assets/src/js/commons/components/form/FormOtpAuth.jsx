c640b71b56474a9fca40c386dbb6b6b6644c39f9
Kai Lam <kailam@4d.com.hk>
2023-04-12 16:55:38 +0800

    otp mobile auth field

---
+++ assets/src/js/commons/components/form/FormOtpAuth.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useFormikContext } from 'formik';
+
+import { faMobileAlt } from '@fortawesome/free-solid-svg-icons/faMobileAlt';
+
+import FormInputGroup from './FormInputGroup';
+import BtnApi from '../btns/BtnApi';
+import FormInput from './FormInput';
+
+const FormOtpAuth = ({
+  api = {
+    url: '/api/otps/auth-mobile',
+    method: 'post',
+  },
+  data = (v) => v.mobile,
+  icon = faMobileAlt,
+  retry = true,
+  children,
+  ...props
+}) => {
+  const { values } = useFormikContext();
+  const d = typeof data === 'function' ? data(values) : data;
+
+  return (
+    <FormInputGroup {...props}>
+      {(p) => (
+        <>
+          <FormInput
+            fieldOnly
+            autoComplete="one-time-code"
+            {...p}
+          />
+          <BtnApi
+            className="btn btn-primary"
+            icon={icon}
+            retry={retry}
+            alertOpts={{ success: false }}
+            api={{
+              ...api,
+              data: { ...api.data, ...d },
+            }}
+          >
+            {children}
+          </BtnApi>
+        </>
+      )}
+    </FormInputGroup>
+  );
+};
+
+export default FormOtpAuth;
