16aca0dcd4a63235227a87f746b15d04a01b487e
Kai Lam <kailam@4d.com.hk>
2023-05-25 16:05:11 +0800

    option to alter create btn

--- assets/src/js/commons/components/list/ctrl/ListCtrlCreate.jsx
+++ assets/src/js/commons/components/list/ctrl/ListCtrlCreate.jsx
@@ -6,12 +6,16 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
 const ListCtrlCreate = ({
   to = 'new',
   className = 'btn px-2 me-3 btn-primary',
+  icon = faPlus,
+  children,
 }) => (
   <Link
     to={to}
     className={className}
   >
-    <FA icon={faPlus} fixedWidth />
+    <FA icon={icon} fixedWidth />
+    {icon && children && ' '}
+    {children}
   </Link>
 );
 
