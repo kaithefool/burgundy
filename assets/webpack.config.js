1a44f94fe4651059c8ddc8a3ab334b2a8ac774a0
Kai Lam <kai.chun.lam@gmail.com>
2025-06-30 23:01:21 +0800

    fix webpack out path

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = ({
-  out = 'public',
+  out = 'public/dist',
 }) => ({
   devtool: 'source-map',
   context: __dirname,
