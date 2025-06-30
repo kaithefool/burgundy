f0aad0ccfaf2d15263a3a0438b0921c926738c78
Kai Lam <kai.chun.lam@gmail.com>
2025-06-30 15:10:46 +0800

    update assets video path

--- assets/src/js/home/components/layout/PageCentered.jsx
+++ assets/src/js/home/components/layout/PageCentered.jsx
@@ -5,7 +5,7 @@ import { Fil } from '~/lib/components/fils';
 import Centered from '~/lib/components/layout/Centered';
 
 const defaultCover = {
-  path: '/assets/img/default-cover.mp4',
+  path: '/dist/assets/img/default-cover.mp4',
   type: 'video/mp4',
 };
 
